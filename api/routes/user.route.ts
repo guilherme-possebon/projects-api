b31a01729300bd5ebeda035a44a26cc821add3c1
guilhermepossebon <guilhermepossebon@webjasper.com.br>
2025-04-23 09:26:19 -0300

    Criando a api

--- api/routes/user.route.ts
+++ api/routes/user.route.ts
@@ -4,10 +4,7 @@ import { UserController } from "../controllers/userController";
 const controller: UserController = new UserController();
 
 const userRouter = Router();
-userRouter.get("/", controller.index);
 userRouter.post("/", controller.store);
 userRouter.get("/:id", controller.show);
-userRouter.put("/:id", controller.update);
-userRouter.delete("/:id", controller.destroy);
 
 export default userRouter;

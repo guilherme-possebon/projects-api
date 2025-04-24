cb6fc6b6bf5f7fcb08444429b38c444ecf3aa028
guilhermepossebon <guilhermepossebon@webjasper.com.br>
2025-04-24 09:47:15 -0300

    teste

--- pages/api/entities/Debug.ts
+++ pages/api/entities/Debug.ts
@@ -22,7 +22,7 @@ export class Debug {
 
   @ManyToOne(() => User, (user) => user.debugs)
   @JoinColumn({ name: "user_id" })
-  user: unknown;
+  user: User;
 
   @CreateDateColumn()
   created_at: Date;
